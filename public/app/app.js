c5e9dbd442ecfaa4be1a043ca7ea59891d2be120
epalva <elroyalva197@hotmail.com>
2015-04-11 15:24:53 +0530

    Working Skeleton for MEAN

---
+++ public/app/app.js
@@ -0,0 +1,16 @@
+angular.module('app', ['ngResource', 'ngRoute']);
+
+angular.module('app').config(function($routeProvider, $locationProvider){
+      $locationProvider.html5Mode({
+        enabled: true
+      });
+      $routeProvider
+        .when('/', { 
+          templateUrl: '/partials/main', 
+          controller: 'mainCtrl'
+         });
+});
+
+angular.module('app').controller('mainCtrl', function($scope){
+    $scope.myVar = "Hello Angular";
+})
---
+++ server.js
@@ -0,0 +1,63 @@
+var express = require('express'),
+    stylus = require('stylus'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    cookieParser = require('cookie-parser'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan')
+    mongoose = require('mongoose');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+function compile(str, path){
+    return stylus(str).set('filename', path);
+}
+
+//Configuration section
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade'); 
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(stylus.middleware(
+    {
+        src: __dirname + '/public',
+        compile: compile
+    }
+));
+app.use(express.static(__dirname + '/public'));
+app.use(cookieParser());
+
+mongoose.connect('mongodb://localhost/nerd-net');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error....'));
+db.once('open', function callback() {
+    console.log('connected to mongodb');
+});
+
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model("Message", messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc){
+    mongoMessage = messageDoc.message;
+});
+
+
+app.get('/partials/:partialPath', function(req, res){
+    res.render('partials/' + req.params.partialPath);
+});
+
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.get('*', function(req,res){
+    res.render('index', {
+    mongoMessage: mongoMessage
+    });
+});
+
+
+var port = 3000;
+app.listen(port);
+
+console.log('Listening on port '+ port + '...');
